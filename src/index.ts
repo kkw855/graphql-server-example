5b160165b3b685455dda125e37de979ae3dd4e83
kkw855 <kkw855@gmail.com>
2024-06-20 22:47:33 +0900

    ✨ feat: Query games, authors, reviews 추가

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,29 @@
-console.log('Hello Apollo Server!')
+import { ApolloServer } from '@apollo/server'
+import { startStandaloneServer } from '@apollo/server/standalone'
+
+import { typeDefs } from './schema'
+import db from './_db'
+
+const PORT = 4000
+
+const resolvers = {
+  Query: {
+    games: () => db.games,
+    authors: () => db.authors,
+    reviews: () => db.reviews,
+  },
+}
+
+// server setup
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+})
+
+const { url } = await startStandaloneServer(server, {
+  listen: {
+    port: PORT,
+  },
+})
+
+console.log(`Server ready at ${url.toString()}:${PORT.toString()}`)
