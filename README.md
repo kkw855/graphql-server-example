93e7b6271451883ec240f877eb7510d36bcf96f5
kkw855 <kkw855@gmail.com>
2024-06-20 21:45:34 +0900

    🎉 init: 프로젝트 생성

---
+++ README.md
@@ -0,0 +1,8 @@
+# Apollo Server
+
+```shell
+pnpm lint       # ESLint warning 또는 error 하나라도 나오면 exit code 1 종료
+pnpm lint:fix   # 모든 파일 ESLint --fix
+pnpm format     # prettier 로 포맷팅 되지 않은 파일이 하나라도 있으면 exit code 1 종료
+pnpm format:fix # prettier 로 모든 파일 포맷팅
+```
---
+++ eslint.config.mjs
@@ -0,0 +1,33 @@
+/* eslint-disable */
+// noinspection JSCheckFunctionSignatures
+
+import eslint from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import eslintConfigPrettier from 'eslint-config-prettier'
+
+export default tseslint.config(
+  {
+    linterOptions: {
+      reportUnusedDisableDirectives: 'error',
+    }
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.strictTypeChecked,
+  ...tseslint.configs.stylisticTypeChecked,
+  {
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    rules: {
+    }
+  },
+  eslintConfigPrettier,
+  {
+    // ESLint 에서 제외할 리스트
+    ignores: ['vite.config.ts', 'tailwind.config.ts'],
+  },
+)
